966508246af95f2dafd4d77d5c3026b883a5f318
Safintim <Sarami96>
2018-10-09 12:56:22 +0300

    fix

--- lesson6.py
+++ lesson6.py
@@ -1,51 +1,28 @@
-from pymonad import curry, State, unit
+from pymonad import curry, State, unit, Nothing
 
 user = {'items': [], 'money': 1000}
-
-items = {
-    'apples': 70,
-    'milk': 80,
-    'wine': 300,
-    'chips': 100
-}
 
 
 @curry
 def buy_item(item, user):
+    items = {
+        'apples': 70,
+        'milk': 80,
+        'wine': 300,
+        'chips': 100
+    }
+
     @State
     def state_computation(money):
         if items.get(item):
-            user['items'].append(item)
             if money - items[item] >= 0:
-                user['money'] = money - items[item]
+                user['items'].append(item)
+                # user['money'] = money - items[item]
                 return [user, money - items[item]]
             else:
-                print('Не хватает мани')
                 return [user, money]
     return state_computation
 
 
-buy = unit(State, user) >> buy_item('wine') >> buy_item('wine') >> buy_item('wine') >> buy_item('wine')
+buy = unit(State, user) >> buy_item('wine') >> buy_item('wine') >> buy_item('wine') >> buy_item('wine') >> buy_item('chips')
 print(buy(user['money']))
-
-
-
-# @curry
-# def buy_item(item_in_store, items):
-#     @State
-#     def state_computation(money):
-#         if items.get(item_in_store):
-#             if money - items[item_in_store] >= 0:
-#                 return [items, money - items[item_in_store]]
-#             else:
-#                 return 'не хватает'
-#         return [items, money]
-#
-#     return state_computation
-#
-#
-# buy = unit(State, items) >> buy_item('') >> buy_item('wine') >> buy_item('wine') >> buy_item('wine') >> buy_item('chips')
-# print(buy(user['money']))
-
-
-
